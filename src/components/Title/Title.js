15583ebaa1924e248cd5be7d9f6be7b09ac3d23d
Payam <payamasem@berkeley.edu>
2018-03-21 10:26:20 -0700

    added title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="titlebox">
+        <h1>Trump Memory Game!</h1>
+        <p>Click on a Trump to earn points, but don't click on any Trump more than once!</p>
+    </div>
+);
+export default Title;
